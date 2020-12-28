a026b50ae9b17d4c69fe8a70f29d17dafcb3a2d8
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2020-12-28 22:36:54 +0300

    added mkdir to script

--- get_model.sh
+++ get_model.sh
@@ -3,6 +3,7 @@
 FILE=models/fasttext/araneum_none_fasttextcbow_300_5_2018.model
 
 if [ ! -f "$FILE" ]; then
+  mkdir -p models/fasttext
   wget https://rusvectores.org/static/models/rusvectores4/fasttext/araneum_none_fasttextcbow_300_5_2018.tgz
   tar -xvzf araneum_none_fasttextcbow_300_5_2018.tgz -C models/fasttext
   rm araneum_none_fasttextcbow_300_5_2018.tgz
